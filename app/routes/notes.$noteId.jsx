daa5050a66cb97790524c9b115ed61a98e9f5536
Philip <philip.alexander.hassialis@gmail.com>
2022-12-20 16:54:03 +0200

    Fetching Data for Dynamic Routes

--- app/routes/notes.$noteId.jsx
+++ app/routes/notes.$noteId.jsx
@@ -1,18 +1,27 @@
-import { Link } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
+import { getStoredNotes } from "data/notes";
 import styles from "~/styles/note-details.css";
 
 const NoteDetailsPage = () => {
+  const note = useLoaderData();
+
   return (
     <main id="note-details">
       <header>
         <nav>
           <Link to="/notes">Back to all Notes</Link>
         </nav>
-        <h1>Note title</h1>
+        <h1>{note.title}</h1>
       </header>
-      <p id="note-details-content">Note conntent</p>
+      <p id="note-details-content">{note.content}</p>
     </main>
   );
+};
+
+export const loader = async ({ params }) => {
+  const notes = await getStoredNotes();
+  const selectedNote = notes.find((note) => note.id === params.noteId);
+  return selectedNote;
 };
 
 export function links() {
